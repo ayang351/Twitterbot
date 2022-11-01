3326c81af2a572b6e3d66e1859c22e9f78edacbf
dograh <52293714+dograh@users.noreply.github.com>
2022-11-01 13:57:12 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'dptc8t8H8jQonMpSVhxQKBjnE',
+  consumer_secret:      'KCEEo8KIS4rurmTVQZUxWEyT3XErK35gGceQHG7w5n0tDCAmFm',
+  access_token:         '1587208704604110850-cjGAL3lmGguHu76qbp5QK4sMdYcm8o',
+  access_token_secret:  'pAB1omohu6a7NUpkOcqWrt9sYNBBk6BRCEYjqiHYz6hbg'
+}
