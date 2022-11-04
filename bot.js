89835bfeb33b496685ba66af7ba32604794dfa2e
Angel Yang <98866739+ayang351@users.noreply.github.com>
2022-11-04 02:03:39 -0400

    code cleanup

--- bot.js
+++ bot.js
@@ -9,20 +9,15 @@ var retweets = {q: "#GordonRamsay", count: 10, result_type: "recent"};
 
 function retweetLatest() {
 	T.get('search/tweets', retweets, function (error, data) {
-	  // log out any errors and responses
 	  console.log(error, data);
-	  // If our search request to the server had no errors...
 	  if (!error) {
-	  	// ...then we grab the ID of the tweet we want to retweet...
 		var retweetId = data.statuses[0].id_str;
-		// ...and then we tell Twitter we want to retweet it!
 		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
 			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
+				console.log('retweet successful')
 			}
-			// If there was an error with our Twitter call, we print it out here.
 			if (error) {
-				console.log('There was an error with Twitter:', error);
+				console.log('retweet unsuccessful', error);
 			}
 		})
 	  }
@@ -33,9 +28,13 @@ function retweetLatest() {
 	});
 }
 
+retweetLatest();
+setInterval(retweetLatest, 1000 * 60 * 60);
+
+var LikeHashtags = ['#GordonRamsay', '#HellsKitchen', '#MasterChef', '#food', '#GordonRamsayRestaurants'];
 // FUNCTION TO LIKE A POST
-var LikeHashtags = ['#GordonRamsay', '#HellsKitchen', '#MasterChef', '#food'];
-var value = Math.floor(Math.random() * LikeHashtags.length)
+function LikePost() {
+var value = Math.floor(Math.random() * LikeHashtags.length);
 
 var likes = {
 	q: LikeHashtags[value],
@@ -50,8 +49,13 @@ T.get('search/tweets', likes,
 		function(err, data, response) {
 			console.log("just liked a post")});
 	console.log(data);
-	}
-) 
+	}) 
+}
+//LikePost function call
+ListPost();
+setInterval(LikePost, 1000 * 60 * 60);
+
+
 
 //this function randomly tweets a gordon ramsay quote (Hemadri's portion)
 var quotes = ["This lamb is so undercooked, it is following Mary to school",
@@ -128,11 +132,3 @@ function FollowBack() {
 //FollowBack function call
 FollowBack();
 setInterval(FollowBack, 1000 * 60 * 60);
-
-
-
-// Try to retweet something as soon as we run the program...
-retweetLatest();
-// ...and then every hour after that. Time here is in milliseconds, so
-// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 60);
